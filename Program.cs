13d07e7121c3ea7aa5f6f926629109c5e42d0689
Erika <Erika1111@yandex.ru>
2023-02-05 11:32:42 +0300

    решение

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string[]inputArray = { "hello", "bay", "good", "1",};
+int arraySize = 0;
+int arrayInd = 0;
+
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if(inputArray[i].Length <= 3) arraySize++;
+}
+string[]Array = new string[arraySize];
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Length <= 3)
+    Array[arrayInd++] = inputArray[i];
+}
+
+Console.Write("Результат: ");
+for (int i = 0; i < Array.Length; i++)
+    Console.Write(Array[i] + ((i !=Array.Length-1) ? ", " : ""));
